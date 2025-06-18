56b0d35b09efaca6c06c4800fa012cf4be8ee340
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-06-18 11:33:14 +0300

    updated interaction.js

--- js/interaction.js
+++ js/interaction.js
@@ -1,6 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/controls/OrbitControls.js';
-
+import * as THREE from 'three';
 
 export function setupInteraction(scene, camera, renderer) {
   const raycaster = new THREE.Raycaster();
