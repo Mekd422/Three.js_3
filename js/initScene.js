adf1776a49253308f0addd8d4ea0d8bd99125512
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-06-18 11:32:39 +0300

    updated initScene.js

--- js/initScene.js
+++ js/initScene.js
@@ -1,12 +1,11 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/controls/OrbitControls.js';
-
-
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 export function initScene() {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100);
   camera.position.set(5, 5, 5);
+  
   const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
@@ -18,8 +17,8 @@ export function initScene() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0x222222);
   });
 
   return { scene, camera, renderer, controls };
 }
+
